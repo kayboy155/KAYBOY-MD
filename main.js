73f6ccf0e083864dcbf77d3fbf104951cf64d305
Kayboy15 <takundaamin074@gmail.com>
2025-05-09 14:57:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'KAYBOY-MD',
             serverMessageId: -1
         }
     }
