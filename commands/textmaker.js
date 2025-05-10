f8664af63eb83e30dbf129d97378876e8329d8e6
Kayboy15 <takundaamin074@gmail.com>
2025-05-10 20:34:57 +0200

    Update textmaker.js

--- commands/textmaker.js
+++ commands/textmaker.js
@@ -6,8 +6,8 @@ const channelInfo = {
     forwardingScore: 1,
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363161513685998@newsletter',
-        newsletterName: 'KnightBot MD',
+        newsletterJid: '83579659263318676@newsletter',
+        newsletterName: 'KAYBOY MD',
         serverMessageId: -1
     }
 };
