d40fd365dfa4ba0e973f0892ffb9c2a5d204b185
Kayboy15 <takundaamin074@gmail.com>
2025-05-10 20:32:05 +0200

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '83579659263318676@newsletter',
+                    newsletterName: 'KAYBOY-MD',
                     serverMessageId: -1
                 }
             }
